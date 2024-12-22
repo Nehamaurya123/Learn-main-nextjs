64079138890e9156af99e882275b25f4d1a5b9db
Neha maurya <nehamaurya7839gmail.com@Nehas-MacBook-Air.local>
2024-12-22 15:29:34 +0530

    first commit

---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,14 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      {/* <NxWelcome title="web" /> */}
+      Hello Next
+    </div>
+  );
+}
+
+export default App;
